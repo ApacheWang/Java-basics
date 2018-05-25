510d8c58cff7cab8fe55a9406669cc09282ce898
water keep wang <15575202581@163.com>
2018-05-25 11:27:05 +0500

    12:27

---
+++ 01Java简介/Demo/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+     实现了一个Java的HelloWorld程序
+	 实现步骤：
+	   1. 定义类
+	   2. 定义主方法
+	   3. 一条命令，控制台输出了HelloWorld
+*/
+public class HelloWorld{
+	//main主方法，固定格式，程序的入口点
+	public static void main(String[] args){
+		//系统 输出 打印    打印的内容
+		System.out.println("HelloWorld");
+	}
+}
